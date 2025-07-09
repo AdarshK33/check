85ae812a4a8a5316f7df4487569b243f1faa954b
Adarsh33K <adarsh.sinha33@gmail.com>
2025-07-09 14:30:11 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import React, {
   useMemo,
   useState,
 } from "react";
-import styles from "./page.module.scss";
+import styles from "./page.module.css";
 import Image from "next/image";
 import ReactFlow, {
   Background,
